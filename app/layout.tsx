03bd8d587da21f45542df968f1e569e6b3bc14bb
Deepak Suthar <imdeepaksuthar@gmail.com>
2025-12-27 01:38:38 +0530

    issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import ScrollProgress from '@/components/ui/ScrollProgress'
 import WhatsAppButton from '@/components/ui/WhatsAppButton'
 import Preloader from '@/components/ui/Preloader'
 import ContentProtection from '@/components/ContentProtection'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -37,7 +36,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.variable} ${manrope.variable} ${spaceGrotesk.variable} font-sans antialiased`}>
-        <SpeedInsights />
         <ContentProtection />
         <CustomCursor />
         <Preloader />
